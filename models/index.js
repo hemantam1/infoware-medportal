d70ef130ba4e7997c63b09da9085d88267f23870
anubhav14g <anubhav1408sks@gmail.com>
2021-03-07 10:37:27 +0530

    connected to remote db

--- models/index.js
+++ models/index.js
@@ -2,27 +2,27 @@ const {Sequelize} = require("sequelize");
 var fs = require("fs");
 var path = require("path");
 
-// const sequelize = new Sequelize('JFmJUEW7AG', 'JFmJUEW7AG', 'uQEwco18y8', {
-//     host: 'remotemysql.com',
-//     port : 3306,
-//     dialect: 'mysql'
-// });
-
-const sequelize = new Sequelize("agri_portal4", "root", "", {
-    define: {
-      charset: "utf8mb4",
-    },
-    host: "localhost",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
-    // logging: false,
-    logging: console.log,
+const sequelize = new Sequelize('JFmJUEW7AG', 'JFmJUEW7AG', 'uQEwco18y8', {
+    host: 'remotemysql.com',
+    port : 3306,
+    dialect: 'mysql'
 });
+
+// const sequelize = new Sequelize("agri_portal4", "root", "", {
+//     define: {
+//       charset: "utf8mb4",
+//     },
+//     host: "localhost",
+//     dialect: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000,
+//     },
+//     // logging: false,
+//     logging: console.log,
+// });
 
 const db = {};
 
